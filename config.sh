fc0c3dd6d235581ae6a5edd2d26732ca18cbc42f
shui-dun <2604697844@qq.com>
2021-05-15 08:10:08 +0800

    update readme

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y trash-cli vim tree net-tools zsh ssh neofetch
+
 for i in $(find $(dirname "$0") -name install.sh); do
   sh $i
 done
--- ssh/install.sh
+++ ssh/install.sh
@@ -4,7 +4,7 @@ sudo apt install -y ssh
 
 sshd_config="/etc/ssh/sshd_config"
 
-# ssh的配置文件是后面的覆盖前面的，因此加到开头
+# ssh的配置文件是前面的覆盖后面的，因此加到开头
 if !(cat $sshd_config | grep -q HAS_BEEN_MODIFIED) ; then
   sudo sh -c "cat $(dirname $0)/my_sshd_config $sshd_config > $sshd_config.tmp"
   sudo mv ${sshd_config}.tmp $sshd_config
