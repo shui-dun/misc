594ac246eea0ab325df9ea3180073da4c3acded7
shui-dun <2604697844@qq.com>
2023-08-16 21:55:42 +0800

    apt自动配置代理

---
+++ apt/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 该文件根据http_proxy和https_proxy为apt设置代理
+
+# apt的配置文件是按字母数字顺序读取的。给文件前缀添加数字可以帮助确定读取的顺序。99proxy是一个命名约定，以确保这个特定的代理配置是最后被读取的，从而覆盖其他可能的设置
+PROXY_CONF="/etc/apt/apt.conf.d/99.conf"
+
+# 判断环境变量是否存在，并写入到文件中
+# -z 用于测试字符串长度是否为零。如果为零（即字符串为空），则返回真。
+if [ ! -z "$http_proxy" ]; then
+    # tee 命令用于读取标准输入并将其写入一个或多个文件。
+    # -a 选项告诉 tee 将输出追加到指定的文件，而不是覆盖它。
+    echo "Acquire::http::Proxy \"$http_proxy\";" | sudo tee -a $PROXY_CONF
+fi
+
+if [ ! -z "$https_proxy" ]; then
+    echo "Acquire::https::Proxy \"$https_proxy\";" | sudo tee -a $PROXY_CONF
+fi
+
+# 如果两个环境变量都不存在，删除文件
+if [ -z "$http_proxy" ] && [ -z "$https_proxy" ]; then
+    sudo rm -f $PROXY_CONF
+fi
+
--- config.sh
+++ config.sh
@@ -2,8 +2,8 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install -y trash-cli vim tree net-tools zsh ssh neofetch
+sudo apt install -y trash-cli vim tree net-tools zsh ssh neofetch htop cmake
 
 for i in $(find $(dirname "$0") -name install.sh); do
-  sh $i
+  bash $i
 done
