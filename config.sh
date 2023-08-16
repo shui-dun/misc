1f394523f46cbd2c539fa680cb9a601c85e45cc7
shui-dun <2604697844@qq.com>
2023-08-16 22:05:07 +0800

    添加软件

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install -y trash-cli vim tree net-tools zsh ssh neofetch htop cmake
+sudo apt install -y trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay cmatrix nodejs docker docker-compose openjdk-17-jdk
 
 for i in $(find $(dirname "$0") -name install.sh); do
   bash $i
