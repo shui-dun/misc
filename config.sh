c4abe72b00f863baa456b2c4701223b4997ef4ca
shui-dun <2604697844@qq.com>
2023-08-21 13:41:05 +0000

    update

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install -y net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay cmatrix nodejs docker docker-compose openjdk-17-jdk
+sudo apt install -y gcc g++ gdb net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay cmatrix nodejs docker docker-compose openjdk-17-jdk
 
 for i in $(find $(dirname "$0") -name install.sh); do
   bash $i
