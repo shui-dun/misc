2e31ce9aceea686797ce59aecfb0f3cdd902cb9f
shui-dun <2604697844@qq.com>
2023-08-25 15:13:21 +0000

    添加ripgrep

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install -y gcc g++ gdb net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay cmatrix nodejs docker docker-compose openjdk-17-jdk silversearcher-ag fzf
+sudo apt install -y gcc g++ gdb net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay cmatrix nodejs docker docker-compose openjdk-17-jdk silversearcher-ag ripgrep fzf
 
 for i in $(find $(dirname "$0") -name install.sh); do
   bash $i
