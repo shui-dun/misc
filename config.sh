85331cb3067c7b8cdba2e49bf5fd9f836dbaf3d4
shui-dun <2604697844@qq.com>
2023-08-26 13:39:44 +0000

    apt upgrade 时不要询问用户

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt install -y gcc g++ gdb net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay cmatrix nodejs docker docker-compose openjdk-17-jdk silversearcher-ag ripgrep fzf
 
 for i in $(find $(dirname "$0") -name install.sh); do
