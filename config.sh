e42ab33b37472ba9daa75737f834ddbcfba14eef
shui-dun <2604697844@qq.com>
2023-09-04 09:16:35 +0000

    添加fd

--- config.sh
+++ config.sh
@@ -5,8 +5,8 @@ sudo apt upgrade -y
 # 安装基本的工具
 # duf是升级版的df
 # ncdu是升级版的du
-# bat是升级版的cat（但调用命令是batcat）
-sudo apt install -y gcc g++ gdb net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay figlet cmatrix nodejs docker docker-compose openjdk-17-jdk silversearcher-ag ripgrep duf ncdu bat
+# bat是升级版的cat
+sudo apt install -y gcc g++ gdb net-tools trash-cli vim tree net-tools zsh ssh neofetch htop cmake sl cowsay figlet cmatrix nodejs docker docker-compose openjdk-17-jdk silversearcher-ag ripgrep fd-find duf ncdu bat
 
 # zsh的安装应该尽可能的早，因为后面的配置可能会修改zsh的配置文件
 bash $(dirname $0)/zsh/install.sh
