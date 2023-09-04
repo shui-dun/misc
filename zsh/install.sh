636242be68faff5f7ec11dd8d80fbc97b7f7875a
shui-dun <2604697844@qq.com>
2023-09-04 00:48:21 +0000

    修改欢迎界面

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,16 +8,19 @@ chsh -s /bin/zsh
 curl -L git.io/antigen > $HOME/.antigen.zsh
 # 复制文件到家目录
 path=$HOME/.zshrc
-sudo cp $(dirname "$0")/zshrc $path
+cp $(dirname "$0")/zshrc $path
 # 如果存在 `http_proxy` 环境变量，写入配置文件开头
 # 之所以要写到开头，是为了确保zsh的插件成功安装
 # 这里使用了 sed 的 -i 选项，它允许直接修改文件
 # "1i表示在第一行之前插入内容
 if [ -n "$http_proxy" ]; then
-    sudo sed -i "1iexport http_proxy=$http_proxy" $path
+    sed -i "1iexport http_proxy=$http_proxy" $path
 fi
 
 # 如果存在 `https_proxy` 环境变量，写入配置文件开头
 if [ -n "$https_proxy" ]; then
-    sudo sed -i "1iexport https_proxy=$https_proxy" $path
+    sed -i "1iexport https_proxy=$https_proxy" $path
 fi
+
+# 将欢迎界面复制到家目录
+cp $(dirname "$0")/welcome $HOME/.welcome
