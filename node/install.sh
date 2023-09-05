a1a5697c999ee8604d9e237d53143c90a143d870
shui-dun <2604697844@qq.com>
2023-09-05 01:48:00 +0000

    添加node的配置文件的注释

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,6 @@
 # 如果node存在并且版本大于等于16，就直接返回
+# tr -d 'v'：删除v，比如v16.0.0 -> 16.0.0
+# cut -d. -f1：以.为分隔符，取第一个字段，比如16.0.0 -> 16
 if command -v node >/dev/null && [[ $(node -v | tr -d 'v' | cut -d. -f1) -ge 16 ]]; then
     echo "node exists and version >= 16"
     exit 0
@@ -8,11 +10,13 @@ fi
 mkdir -p /opt/node
 wget -O /opt/node.tar.xz https://nodejs.org/dist/v18.17.1/node-v18.17.1-linux-x64.tar.xz
 # 解压（并命名为node）
+# --strip-components 1：去掉第一层目录(node-v18.17.1-linux-x64)
 tar -xf /opt/node.tar.xz -C /opt/node --strip-components 1
 # 删除压缩包
 rm -f /opt/node.tar.xz
 
 # 为bash和zsh分别设置环境变量
+# /etc/profile是bash会执行的，但是zsh不会
 echo 'export PATH=/opt/node/bin:$PATH' >> /etc/profile
 mkdir -p /etc/zsh
 echo 'export PATH=/opt/node/bin:$PATH' >> /etc/zsh/zshrc
