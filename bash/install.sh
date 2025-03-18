4f8604e87ffd61879b0ff9f1f3993a8e6d073d01
shui-dun <2604697844@qq.com>
2025-03-18 12:57:38 +0800

    bashrc配置代理

--- bash/install.sh
+++ bash/install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# 定义配置文件路径
+CONFIG_NAME=.shuidun-bash-config.sh
+CONFIG_FILE=~/$CONFIG_NAME
+
 # 将配置文件复制到主目录
-cp $(dirname "$0")/.shuidun-bash-config ~/
+cp $(dirname "$0")/$CONFIG_NAME $CONFIG_FILE
+
+# 配置代理设置
+echo "export http_proxy=\"${http_proxy}\"" >> $CONFIG_FILE
+echo "export https_proxy=\"${https_proxy}\"" >> $CONFIG_FILE
 
 # 将 source 行定义为变量
-SOURCE_LINE="source ~/.shuidun-bash-config"
+SOURCE_LINE="source $CONFIG_FILE"
 
 # 如果 .bashrc 中不存在 source 行，则添加
 if ! grep -q "$SOURCE_LINE" ~/.bashrc; then
