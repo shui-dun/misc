68feca686cfdf6511a0b828b8dad8a28ba0934c5
shui-dun <2604697844@qq.com>
2025-03-26 14:49:20 +0800

    curl静默执行

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,7 @@ sudo apt install -y vim
 # 复制vimrc文件到用户目录
 cp $(dirname "$0")/vimrc ~/.vimrc
 # 安装vim插件管理器
+# -sS 使得curl不会将进度条输出到stderr，但输出错误信息到stderr
 curl -sS -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # 安装vim插件
